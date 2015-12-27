aed1a0eb51f48c02b2917b4f19c49989e6bba03e
shagufta <shagufta@ironqabinet.com>
2015-12-28 02:01:42 +0530

    md

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,8 +18,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
- app.import('bower_components/material-design-lite/material.min.css');
-  app.import('bower_components/material-design-lite/material.min.js');
-
+ app.import(app.bowerDirectory + '/material-design-lite/material.css');
   return app.toTree();
 };
